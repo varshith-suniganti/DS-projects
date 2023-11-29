4cdb4d4f33236507c8bf86a16212117a9f9a855b
varshith-suniganti <150051419+varshith-suniganti@users.noreply.github.com>
2023-11-29 10:19:40 +0530

    Add files via upload

---
+++ Queue.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 15
+void enqueue();
+void dequeue();
+void display();
+ 
+ int queue[MAX];
+ int rear=-1,front=0;
+  
+void main()
+{
+	while(1)
+	{
+	int choice;
+	printf("enter the your choice:\n");
+	printf("1.insert\n2.delete\n3.display\n4.exit\n");
+	scanf("%d",&choice);
+	     
+		switch(choice)
+	    {
+		case 1:enqueue();break;
+		case 2:dequeue();break;
+		case 3:display();break;
+		case 4:exit(0);
+		default:printf("invalid choice!!");
+		}
+    }
+}
+
+void enqueue()
+{
+	int x;
+	if(rear==MAX-1)
+	{
+		printf("queue is full!!\n");
+	}
+	printf("enter the element:");
+	scanf("%d",&x);
+	rear++;
+	queue[rear]=x;
+}
+ void dequeue()
+ {
+ 	if(rear==-1 && front==0)
+ 	 {
+ 		printf("queue is empty!!\n");
+	 }
+	 else if(front==rear)
+	 {
+	 	printf("deleted element is %d",queue[front]);
+	 	front=0;
+	 	rear=-1;
+	 }
+	 else
+	 {
+	 	printf("deleted element is %d",queue[front]);
+	    front++;
+	 }
+ }
+void display()
+{
+	int i;
+	if(rear==-1 && front==0)
+	{
+		printf("queue is empty!");
+	}
+		else
+		{
+			for( i=front;i<=rear;i++)
+			printf("%d\t",queue[i]);
+			printf("\n");
+		}
+}
+
+
+
+
+
+
+
+
+
+
---
+++ stack.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 6
+
+  void push();
+  void pop();
+  void peek();
+  void display();
+  
+ int top=-1;
+ int stack[MAX];
+ 
+ 
+ void main()
+ {
+ 	while(1) 
+	{
+ 	int choice;
+	 printf("enter your choice:\n");
+ 	printf("1.push\n2.pop\n3.peek\n4.display\n5.exit\n");
+ 	scanf("%d",&choice);
+ 		
+		 switch(choice)
+		{
+		 	case 1:push();break;
+			case 2:pop();break;
+			case 3:peek();break;
+			case 4:display();break;
+			case 5:exit(0);	
+			default:printf("invalid choice");	  
+		}		
+	}
+ } 
+   void push()
+   {
+   	int x;
+   	printf("enter the number:\n");
+   	scanf("%d",&x);
+   	if(top==MAX-1)
+   	{
+   		printf("stack is overflow!!");
+	   }
+	   else{
+	   	top++;
+	   	stack[top]=x;
+	   }
+   }
+   	void pop()
+   	{
+   		if(top>0)
+   		{
+   			return;
+   	    } 
+	}
+	void peek()
+	{
+		printf("%d",stack[top]);
+	}
+	 void display()
+	 {
+	 	int i;
+	 	for(i=0;i<=top;i++)
+	 	{
+	 		printf("%d\t",stack[i]);
+		 }
+	 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
